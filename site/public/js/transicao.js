6a9081830692a63de6a3f0e2a8cc0d4a6cf5b739
MRESENDESILVA <mateus.rsilva@sptech.school>
2023-03-05 12:59:53 -0300

    Update/add - atualizando cor e posição da header mobile e criando arquivo de teste

--- site/public/js/transicao.js
+++ site/public/js/transicao.js
@@ -1,56 +1,29 @@
-window.addEventListener('scroll', () => {
-    const background = document.getElementById("background")
-    let posicoes = background.getBoundingClientRect();
+// window.addEventListener('scroll', () => {
+//     const background = document.getElementById("background")
+//     let posicoes = background.getBoundingClientRect();
 
-    if (posicoes.top <= 70) {
+//     if (posicoes.top <= 70) {
 
-        const element = document.getElementsByClassName("link");
-        for (let index = 0; index < element.length; index++) {
-            element[index].classList.add("after");
-        }
-        for (let index = 0; index < element.length; index++) {
-            element[index].classList.remove("link");
-        }
-    } else {
+//         const element = document.getElementsByClassName("link");
+//         for (let index = 0; index < element.length; index++) {
+//             element[index].classList.add("after");
+//         }
+//         for (let index = 0; index < element.length; index++) {
+//             element[index].classList.remove("link");
+//         }
+//     } else {
 
-        const element = document.getElementsByClassName("after");
-        for (let index = 0; index < element.length; index++) {
-            element[index].classList.add("link");
-        }
-        for (let index = 0; index < element.length; index++) {
-            element[index].classList.remove("after");
-        }
-    }
+//         const element = document.getElementsByClassName("after");
+//         for (let index = 0; index < element.length; index++) {
+//             element[index].classList.add("link");
+//         }
+//         for (let index = 0; index < element.length; index++) {
+//             element[index].classList.remove("after");
+//         }
+//     }
 
-})
+// })
 
-window.addEventListener('resize', (event) => {
-
-    const element = document.getElementById("header")
-    console.log(element);
-    if (window.innerWidth < 1000) {
-        element.classList.add("row-reverse");
-        element.classList.remove("row");
-
-    } else {
-        element.classList.add("row");
-        element.classList.remove("row-reverse");
-
-    }
-})
-
-function redimensionar() {
-    const element = document.getElementById("header")
-    console.log(element);
-    if (window.innerWidth < 1201) {
-        element.classList.add("row-reverse");
-        element.classList.remove("row");
-
-    } else {
-        element.classList.add("row");
-        element.classList.remove("row-reverse");
-    }
-}
 
 function openClose(n) {
     if (n == 1) {
