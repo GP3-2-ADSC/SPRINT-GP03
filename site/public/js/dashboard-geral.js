4f93e1c5b496370e59c9a1f50c1714fa584f1d75
luizfiller <luiz.fsilva@sptech.school>
2023-04-15 03:56:42 -0300

    Add - término de rafatoração de tela de dash geral

--- site/public/js/dashboard-geral.js
+++ site/public/js/dashboard-geral.js
@@ -1,81 +1,112 @@
-var ctx1 = document.getElementById('grafico1').getContext('2d');
-var chart1 = new Chart(ctx1, {
-    type: 'doughnut',
-    data: {
-        labels: ['Uso de Rede'],
-        datasets: [{
-            data: [60, 40],
-            backgroundColor: ['#F44336', '#FAEBD7']
-        }]
+// ChartJS da KPI da CPU
+const data_grafico1 = {
+  labels: ["Rede disponível", "Uso da rede"],
+  datasets: [
+    {
+      data: [100 - 72, 72],
+      backgroundColor: ["#d0d0d0", "#F44336"],
+      borderColor: "#b8b8b8",
+      hoverOffset: 4,
+      cutout: "50%",
     },
-    options: {
-        cutoutPercentage: 90,
-        responsive: false,
-        maintainAspectRatio: false,
-        tooltips: {
-            enabled: false
-        },
-        legend: {
-            display: false
-        },
-        animation: {
-            animateScale: true,
-            animateRotate: true
-        }
-    }
-});
+  ],
+};
 
-var ctx2 = document.getElementById('grafico2').getContext('2d');
-var chart2 = new Chart(ctx2, {
-    type: 'doughnut',
-    data: {
-        labels: ['Uso de Rede'],
-        datasets: [{
-            data: [30, 70],
-            backgroundColor: ['#F44336', '#FAEBD7']
-        }]
+const config_grafico1= {
+  type: "doughnut",
+  data: data_grafico1,
+  options: {
+    responsive: false,
+    plugins: {
+      legend: {
+        display: false,
+      },
     },
-    options: {
-        cutoutPercentage: 90,
-        responsive: false,
-        maintainAspectRatio: false,
-        tooltips: {
-            enabled: false
-        },
-        legend: {
-            display: false
-        },
-        animation: {
-            animateScale: true,
-            animateRotate: true
-        }
-    }
-});
+  },
+};
 
-var ctx3 = document.getElementById('grafico3').getContext('2d');
-var chart3 = new Chart(ctx3, {
-    type: 'doughnut',
-    data: {
-        labels: ['Uso de Rede'],
-        datasets: [{
-            data: [80, 20],
-            backgroundColor: ['#F44336', '#FAEBD7']
-        }]
+const myChart_kpi1 = new Chart(
+  document.getElementById("grafico1"),
+  config_grafico1
+);
+
+
+
+const data_grafico2 = {
+    labels: ["RAM disponível", "Uso da RAM"],
+    datasets: [
+      {
+        data: [34, 100 - 34],
+        backgroundColor: ["#d0d0d0", "#F44336"],
+        borderColor: "#b8b8b8",
+        hoverOffset: 4,
+        cutout: "50%",
+      },
+    ],
+  };
+
+  const config_grafico2 = {
+    type: "doughnut",
+    data: data_grafico2,
+    options: {
+      responsive: false,
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
     },
-    options: {
-        cutoutPercentage: 90,
-        responsive: false,
-        maintainAspectRatio: false,
-        tooltips: {
-            enabled: false
-        },
-        legend: {
-            display: false
-        },
-        animation: {
-            animateScale: true,
-            animateRotate: true
-        }
-    }
-});
+  };
+  
+  const myChart_kpi2 = new Chart(
+    document.getElementById("grafico2"),
+    config_grafico2
+  );
 
+  
+
+  const data_grafico3 = {
+    labels: ["CPU disponível", "Uso da CPU"],
+    datasets: [
+      {
+        data: [20,92],
+        backgroundColor: ["#d0d0d0", "#F44336"],
+        borderColor: "#b8b8b8",
+        hoverOffset: 4,
+        cutout: "50%",
+      },
+    ],
+  };
+  
+  const config_grafico3 = {
+    type: "doughnut",
+    data: data_grafico3,
+    options: {
+      responsive: false,
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
+    },
+  };
+  
+  const myChart_kpi3 = new Chart(
+    document.getElementById("grafico3"),
+    config_grafico3
+  );
+
+
+  //<![CDATA[
+var ttChatLoaderS = document.createElement('script');
+document.tomticketChatLoaderScriptVersion = 2;
+ttChatLoaderS.src = 'https://retria.tomticket.com/scripts-chat/chat.min.js'
++ '?id=EP61558'
++ '&account=3939712P05042023082156'
++ '&autoOpen=0'
++ '&hideWhenOffline=0'
++ '&d=retria'
++ '&ts=' + new Date().getTime()
++ '&ref=' + encodeURIComponent(document.URL);
+document.body.appendChild(ttChatLoaderS);
+//]]>
